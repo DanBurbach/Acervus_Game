1e2b2f1c3002bb8eb0c30a16bd209f67fe40b6c4
DanBurbach <dburbach1982@gmail.com>
2019-10-04 09:47:31 -0700

    refactor file and folder layout for modular usability

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./../../assets/Main.css";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>hey</div>
+  }
+}
+export default Main;
