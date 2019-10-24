b8ab2ea80b0ffb5b565e53c1f65fd6a63cc51e8f
DanBurbach <dburbach1982@gmail.com>
2019-10-23 23:58:14 -0700

    change sizing of generated objects

--- src/components/Main/circle.jsx
+++ src/components/Main/circle.jsx
@@ -3,7 +3,7 @@ import Matter, { Common } from "matter-js";
 export default function Circle() {
   let x = 30;
   let y = 30;
-  let r = Common.random(30);
+  let r = Common.random(5, 40);
   this.body = Matter.Bodies.circle(x, y, r, {
     friction: 0.1,
     restitution: 0.9
--- src/components/Main/rectangle.jsx
+++ src/components/Main/rectangle.jsx
@@ -4,8 +4,8 @@ export default function polygon(x, y) {
   this.body = Matter.Bodies.rectangle(
     90,
     50,
-    Common.random(10, 70),
-    Common.random(12, 90),
+    Common.random(9, 90),
+    Common.random(12, 120),
     { restitution: 0.5, 
       friction: 0.1 }
   );
