45c2db091786dfdb4eb9012012c5451c78ec26ff
DanBurbach <dburbach1982@gmail.com>
2019-10-31 23:45:38 -0700

    create Main Title page utilizing Google fonts, history to button, and modify styling to match other pages with 8 bit style

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+// import { Route } from "react-router-dom";
+
+// import * as ROUTES from "../../constants/routes";
+// import Easy from "../Easy";
+
+import '../../assets/Title.css'
+
+class Title extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.easyGame = this.easyGame.bind(this);
+  }
+
+easyGame = () =>{
+    this.props.history.push('/Easy');
+}
+
+render() {
+    return (
+        <div className="title_main_container">
+            <ul>
+                <ol>
+                    <h1>Acervus</h1>
+                </ol>
+                <ol>
+                    <div className="title_definition">
+                        A multitude of objects of the same kind, rising in a heap...
+                    </div>
+                </ol>
+                <ol>
+                    <button
+                        className="title_easy_game"
+                        onClick={this.easyGame}
+                    >Easy Game</button>
+                </ol>
+            </ul>
+        </div>
+    );
+}
+}
+
+export default Title;
