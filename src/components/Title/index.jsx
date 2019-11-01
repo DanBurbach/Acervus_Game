11a3cf6fab9a248362dde28313c64ba20a6aa2de
DanBurbach <dburbach1982@gmail.com>
2019-11-01 00:09:53 -0700

    create button for Hard Game and pathing

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-// import { Route } from "react-router-dom";
-
-// import * as ROUTES from "../../constants/routes";
-// import Easy from "../Easy";
 
 import '../../assets/Title.css'
 
@@ -11,32 +7,41 @@ class Title extends Component {
     super(props);
     this.state = {};
     this.easyGame = this.easyGame.bind(this);
+    this.hardGame = this.hardGame.bind(this);
   }
 
 easyGame = () =>{
     this.props.history.push('/Easy');
 }
 
+hardGame = () => {
+    this.props.history.push('./Hard');
+}
+
 render() {
     return (
-        <div className="title_main_container">
-            <ul>
-                <ol>
-                    <h1>Acervus</h1>
-                </ol>
-                <ol>
-                    <div className="title_definition">
-                        Def: A multitude of objects of the same kind, rising in a heap...
-                    </div>
-                </ol>
-                <ol>
-                    <button
-                        className="title_easy_game"
-                        onClick={this.easyGame}
-                    >Easy Game</button>
-                </ol>
-            </ul>
-        </div>
+      <div className="title_main_container">
+        <ul>
+          <ol>
+            <h1>Acervus</h1>
+          </ol>
+          <ol>
+            <div className="title_definition">
+              Def: A multitude of objects of the same kind, rising in a heap...
+            </div>
+          </ol>
+          <ol>
+            <button className="title_easy_game" onClick={this.easyGame}>
+              Easy Game
+            </button>
+          </ol>
+          <ol>
+            <button className="title_hard_game" onClick={this.hardGame}>
+              Hard Game
+            </button>
+          </ol>
+        </ul>
+      </div>
     );
 }
 }
