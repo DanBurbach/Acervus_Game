ec8214076c129021c107c5ab72b8105d68112a78
DanBurbach <dburbach1982@gmail.com>
2019-11-12 21:44:28 -0800

    grey out all code, set aside for reference but may delete at a later date

--- src/components/Impossible/test.jsx
+++ src/components/Impossible/test.jsx
@@ -1,138 +1,138 @@
-import React, { Component } from "react";
-import Matter, { Example } from "matter-js";
+// import React, { Component } from "react";
+// import Matter, { Example } from "matter-js";
 
-import "./../../assets/Easy.css";
-import "./../../assets/gameEnd.css";
-import "./../../assets/crt.css";
+// import "./../../assets/Easy.css";
+// import "./../../assets/gameEnd.css";
+// import "./../../assets/crt.css";
 
-class Test extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+// class Test extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
 
-  componentDidMount() {
-        // var Example = Example || {};
+//   componentDidMount() {
+//         // var Example = Example || {};
 
-        Example.staticFriction = function() {
-        var Engine = Matter.Engine,
-            Render = Matter.Render,
-            Runner = Matter.Runner,
-            Body = Matter.Body,
-            Composites = Matter.Composites,
-            Events = Matter.Events,
-            MouseConstraint = Matter.MouseConstraint,
-            Mouse = Matter.Mouse,
-            World = Matter.World,
-            Bodies = Matter.Bodies;
+//         Example.staticFriction = function() {
+//         var Engine = Matter.Engine,
+//             Render = Matter.Render,
+//             Runner = Matter.Runner,
+//             Body = Matter.Body,
+//             Composites = Matter.Composites,
+//             Events = Matter.Events,
+//             MouseConstraint = Matter.MouseConstraint,
+//             Mouse = Matter.Mouse,
+//             World = Matter.World,
+//             Bodies = Matter.Bodies;
 
-        // create engine
-        var engine = Engine.create(),
-            world = engine.world;
+//         // create engine
+//         var engine = Engine.create(),
+//             world = engine.world;
 
-        // create renderer
-        var render = Render.create({
-            element: document.body,
-            engine: engine,
-            options: {
-            width: 800,
-            height: 600,
-            showVelocity: true
-            }
-        });
+//         // create renderer
+//         var render = Render.create({
+//             element: document.body,
+//             engine: engine,
+//             options: {
+//             width: 800,
+//             height: 600,
+//             showVelocity: true
+//             }
+//         });
 
-        Render.run(render);
+//         Render.run(render);
 
-        // create runner
-        var runner = Runner.create();
-        Runner.run(runner, engine);
+//         // create runner
+//         var runner = Runner.create();
+//         Runner.run(runner, engine);
 
-        // add bodies
-        var body = Bodies.rectangle(400, 500, 200, 60, {
-            isStatic: true,
-            chamfer: 10
-            }),
-            size = 50,
-            counter = -1;
+//         // add bodies
+//         var body = Bodies.rectangle(400, 500, 200, 60, {
+//             isStatic: true,
+//             chamfer: 10
+//             }),
+//             size = 50,
+//             counter = -1;
 
-        var stack = Composites.stack(350, 470 - 6 * size, 1, 6, 0, 0, function(x, y) {
-            return Bodies.rectangle(x, y, size * 2, size, {
-            slop: 0.5,
-            friction: 1,
-            frictionStatic: Infinity
-            });
-        });
+//         var stack = Composites.stack(350, 470 - 6 * size, 1, 6, 0, 0, function(x, y) {
+//             return Bodies.rectangle(x, y, size * 2, size, {
+//             slop: 0.5,
+//             friction: 1,
+//             frictionStatic: Infinity
+//             });
+//         });
 
-        World.add(world, [
-            body,
-            stack,
-            // walls
-            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-            Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-            Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-        ]);
+//         World.add(world, [
+//             body,
+//             stack,
+//             // walls
+//             Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+//             Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+//             Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+//             Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+//         ]);
 
-        Events.on(engine, "beforeUpdate", function(event) {
-            counter += 0.014;
+//         Events.on(engine, "beforeUpdate", function(event) {
+//             counter += 0.014;
 
-            if (counter < 0) {
-            return;
-            }
+//             if (counter < 0) {
+//             return;
+//             }
 
-            var px = 400 + 100 * Math.sin(counter);
+//             var px = 400 + 100 * Math.sin(counter);
 
-            // body is static so must manually update velocity for friction to work
-            Body.setVelocity(body, { x: px - body.position.x, y: 0 });
-            Body.setPosition(body, { x: px, y: body.position.y });
-        });
+//             // body is static so must manually update velocity for friction to work
+//             Body.setVelocity(body, { x: px - body.position.x, y: 0 });
+//             Body.setPosition(body, { x: px, y: body.position.y });
+//         });
 
-        // add mouse control
-        var mouse = Mouse.create(render.canvas),
-            mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                visible: false
-                }
-            }
-            });
+//         // add mouse control
+//         var mouse = Mouse.create(render.canvas),
+//             mouseConstraint = MouseConstraint.create(engine, {
+//             mouse: mouse,
+//             constraint: {
+//                 stiffness: 0.2,
+//                 render: {
+//                 visible: false
+//                 }
+//             }
+//             });
 
-        World.add(world, mouseConstraint);
+//         World.add(world, mouseConstraint);
 
-        // keep the mouse in sync with rendering
-        render.mouse = mouse;
+//         // keep the mouse in sync with rendering
+//         render.mouse = mouse;
 
-        // fit the render viewport to the scene
-        Render.lookAt(render, {
-            min: { x: 0, y: 0 },
-            max: { x: 800, y: 600 }
-        });
+//         // fit the render viewport to the scene
+//         Render.lookAt(render, {
+//             min: { x: 0, y: 0 },
+//             max: { x: 800, y: 600 }
+//         });
 
-        // context for MatterTools.Demo
-        return {
-            engine: engine,
-            runner: runner,
-            render: render,
-            canvas: render.canvas,
-            stop: function() {
-            Matter.Render.stop(render);
-            Matter.Runner.stop(runner);
-            }
-        };
-        };
+//         // context for MatterTools.Demo
+//         return {
+//             engine: engine,
+//             runner: runner,
+//             render: render,
+//             canvas: render.canvas,
+//             stop: function() {
+//             Matter.Render.stop(render);
+//             Matter.Runner.stop(runner);
+//             }
+//         };
+//         };
 
-        if (typeof module !== "undefined") {
-        module.exports = Example[Object.keys(Example)[0]];
-        }
-};
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
+//         if (typeof module !== "undefined") {
+//         module.exports = Example[Object.keys(Example)[0]];
+//         }
+// };
+//   render() {
+//     return (
+//       <div>
+//       </div>
+//     );
+//   }
+// }
 
-export default Test;
+// export default Test;
